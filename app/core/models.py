a4f713a457ae85b259a7e361d26f180c85b96fbc
tuananh1001 <trantuananh100119999@gmail.com>
2020-04-18 10:32:54 +0700

    test new superuser

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email= self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self,email,password):
+
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,4 +15,25 @@ class ModelTests(TestCase):
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
 
-        #Day la phan duoc them vao
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(email,'test123')
+
+        self.assertEqual(user.email,email.lower())
+    
+    def test_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
+
+    def test_create_new_superuser(self):
+
+        user = get_user_model().objects.create_superuser(
+            'test@abc',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
